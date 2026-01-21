0157aa1864adb48b8150d65fc85b4b2a4354b8c9
animeshPi <singhanimesh.pi@gmail.com>
2026-01-21 22:03:08 +0530

    lspconfig

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,14 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
+local servers = {
+  "html",
+  "cssls",
+  "rust_analyzer",
+  "gopls",
+  "pyright",
+  "bashls",
+  "tsserver",
+}
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
