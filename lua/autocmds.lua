3be38ea92aa9a47be80c324e05054ad8440c04f1
animeshPi <singhanimesh.pi@gmail.com>
2026-07-26 11:19:46 +0530

    made the nvimtree on focus by default

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,1 +1,26 @@
 require "nvchad.autocmds"
+
+local api = vim.api
+
+api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    local argc = vim.fn.argc()
+
+    if argc == 0 then
+      vim.schedule(function()
+        vim.cmd("NvimTreeFocus")
+      end)
+      return
+    end
+
+    if argc == 1 then
+      local arg = vim.fn.argv(0) --[[@as string]]
+
+      if vim.fn.isdirectory(arg) == 1 then
+        vim.schedule(function()
+          vim.cmd("NvimTreeFocus")
+        end)
+      end
+    end
+  end,
+})
